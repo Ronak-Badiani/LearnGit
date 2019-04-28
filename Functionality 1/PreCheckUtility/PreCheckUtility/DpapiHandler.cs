6ecad4b82a3a3b8a33025efacc67d551be0fc7f0
Ronak Manojbhai Badiani <ronakbadiani6@gmail.com>
2019-04-28 12:24:01 +0530

    Added Functionality 1

---
+++ Functionality 1/PreCheckUtility/PreCheckUtility/DpapiHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace PreCheckUtility
+{
+    using System;
+    using System.Security.Cryptography;
+    using System.Text;
+    public static class DpapiHandler
+    {
+        private static readonly byte[] sEntropy = { 1, 2, 3, 4, 5, 6 };
+
+        public static string Encrypt(string text, DataProtectionScope scope)
+        {
+            // first, convert the text to byte array 
+            byte[] originalText = Encoding.Unicode.GetBytes(text);
+
+            // then use Protect() to encrypt your data 
+            byte[] encryptedText = ProtectedData.Protect(originalText, sEntropy, scope);
+
+            //and return the encrypted message 
+            return Convert.ToBase64String(encryptedText);
+        }
+
+        public static string Decrypt(string text, DataProtectionScope scope)
+        {
+            // the encrypted text, converted to byte array 
+            byte[] encryptedText = Convert.FromBase64String(text);
+
+            // calling Unprotect() that returns the original text 
+            byte[] originalText = ProtectedData.Unprotect(encryptedText, sEntropy, scope);
+
+            // finally, returning the result 
+            return Encoding.Unicode.GetString(originalText);
+        }
+    }
+}
---
+++ Functionality 1/PreCheckUtility/PreCheckUtility/Program.cs
@@ -0,0 +1,25 @@
+﻿/// <summary>
+/// Program to precheck the keys before pulling data from upstream  
+/// </summary>
+namespace PreCheckUtility
+{
+    using Logger;
+    using System;
+
+    class Program
+    {
+        internal static string errorValue;
+        internal static ILogger loggerDetails = InitializeLogger.GetEventLogger(new EventLoggerConfig(Constants.APPLICATION, Constants.ARGUMENTS, Constants.EVENT_ID)); // EventViewer returns error with ID 108
+        internal static string logFilePath = string.Concat(Environment.CurrentDirectory, Constants.LOG_FILE_PATH);
+
+        static int Main(string[] args)
+        {
+            Helper.SetOutputWindow(logFilePath);
+            if (Helper.ReadXML() == 1)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
